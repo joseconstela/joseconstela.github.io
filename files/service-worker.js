ee3a83a6165af1c1d5358e15fe62663bdbabea9b
Jose Constela <jose@joseconstela.com>
2021-01-03 17:23:53 +0100

    service worker

---
+++ files/service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = 'jcCache'
+const filesToCache = [
+  '/',
+  '/files/print.css',
+  '/files/style.css',
+  '/index.html'
+]
+
+self.addEventListener('activate', e => self.clients.claim())
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+  )
+})
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(response => response ? response : fetch(e.request))
+  )
+})
